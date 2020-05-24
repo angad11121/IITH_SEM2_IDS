e0c37e6a1a5863da1afaf4678993f79f974854ed
Gunangad Pal Singh Narula <gapsnarula@gmail.com>
2020-05-24 19:23:12 +0530

    Done

--- Project_Assignment.c
+++ Project_Assignment.c
@@ -3,12 +3,15 @@
 #include <stdbool.h>
 #include <time.h>
 
+// Constant Parameters
 #define MAX_VERTICES 10000
 #define MAX_EDGES 3000
 #define DAYS 300
 #define TAU 0.5
 #define GAMMA 0.2
 
+
+// Some variables as global parameters for the simulation.
 int curr_day = 0;
 int susceptible;
 int infected;
@@ -18,13 +21,15 @@ int recovered;
 	Plague Inc. 
 	(Low-Cost Version)
 */
+
+// A boolean matrix to store adjacent nodes
 bool neighbour[MAX_VERTICES][MAX_VERTICES];
 
+// An event node.
 struct q_node
 {
 	int id;
 	int day;
-
 	char action;
 	/*
 		T - Transmit
@@ -33,6 +38,7 @@ struct q_node
 	struct q_node *next;
 };
 
+// A Linked List of the event nodes.
 struct queue
 {
 	int length;
@@ -42,12 +48,13 @@ struct queue
 events;
 
 
+// Structure of a graph node
 struct vertex
 {
 	int id;
 	char status;
-	int pred_inf_time;
-	int rec_time;
+	int pred_inf_time; // Pedicted infection time
+	int rec_time;  // Recovery time
 
 	struct l_node *adjacent;
 }
@@ -61,7 +68,7 @@ graph[MAX_VERTICES];
 */
 
 
-// Function to add jobs to the end of queue.
+// Function to add event to the priority queue.
 void enqueue(struct queue *event_queue, int id, int day, char action)
 {
 	// Memory Allocation to a node
@@ -73,7 +80,7 @@ void enqueue(struct queue *event_queue, int id, int day, char action)
     	exit(1); 
 	}
 	
-	// Initializing Values Of the Job (Node)
+	// Initializing Values Of the Event (Node)
 	newnode->id = id;
 	newnode->day = day;
 	newnode->action = action;
@@ -107,7 +114,7 @@ void enqueue(struct queue *event_queue, int id, int day, char action)
 	event_queue->length +=1;
 }
 
-// Function to remove job from the front of queue
+// Function to remove event from the front of queue
 void dequeue(struct queue *event_queue)
 {
 	// Error handling
@@ -133,8 +140,10 @@ void dequeue(struct queue *event_queue)
 }
 
 
+// Function to create the graph.
 void create_graph(int vertices)
 {
+	// Making the nodes disjoint.
 	for (int i = 0; i < vertices; ++i)
 	{
 		for (int j = 0; j < vertices; ++j)
@@ -143,14 +152,17 @@ void create_graph(int vertices)
 		}
 	}
 
+
 	for (int i = 0; i < vertices; ++i)
 	{
+		// Initialising the node values 
 		graph[i].id = i;
 		graph[i].status = 'S';
 		graph[i].pred_inf_time = DAYS+1;
 		graph[i].rec_time = DAYS+1;
 		graph[i].adjacent = NULL;
 	
+		// Making a directed graph by joining the links
 		int node_neighbours[MAX_EDGES];
 		for (int j = 0; j < MAX_EDGES; ++j)
 		{
@@ -163,8 +175,11 @@ void create_graph(int vertices)
 		}
 	}
 
+	// Global Variable Initialised
 	susceptible = vertices;
 
+	// Converting directed graph to undirected.
+	// Using symmetry.
 	for (int i = 0; i < vertices; ++i)
 	{
 		for (int j = i; j < vertices; ++j)
@@ -174,6 +189,8 @@ void create_graph(int vertices)
 	}
 }
 
+// Prints the graph
+// ##Not a mandatory funtion as per the project, but for user convenience.
 void printgraph(int vertices)
 {
 	for (int i = 0; i < vertices; ++i)
@@ -195,22 +212,22 @@ int min(int a, int b)
 	else return a;
 }
 
-
+// The Coin-Flip Experiment.
 int exponent_variate(float lambda)
 {
 	int tries = 1;
 	int norm_lam = (int)(lambda*10);
-	// printf("%d\n", norm_lam);
 	int ran = rand()%10;
 	while(ran >= norm_lam)
 	{
-		// printf("%d\n",tries );
 		tries++;
 		ran = rand()%10;
 	}
 	return tries;
 }
 
+// Funtion as Given in Project PDF
+// Function for predicting the infection time of targets
 void find_trans_SIR(struct queue *event_queue, struct vertex *target, struct vertex *source)
 {
 	if((target->status) == 'S')
@@ -224,6 +241,8 @@ void find_trans_SIR(struct queue *event_queue, struct vertex *target, struct ver
 	}
 }
 
+// Funtion as Given in Project PDF
+// Function for infecting the targets.
 void proc_trans_SIR(struct queue *event_queue, struct vertex *target, int vertices)
 {
 	if((target->status) == 'S')
@@ -248,6 +267,8 @@ void proc_trans_SIR(struct queue *event_queue, struct vertex *target, int vertic
 	dequeue(event_queue);
 }
 
+// Funtion as Given in Project PDF
+// Function for recovering the infected people.
 void proc_rec_SIR(struct queue *event_queue,struct vertex *target)
 {
 	if((target->status) == 'I')
@@ -259,13 +280,17 @@ void proc_rec_SIR(struct queue *event_queue,struct vertex *target)
 	dequeue(event_queue);
 }
 
+// Funtion as Given in Project PDF
+// Processing the event queue/ running the simulation.
 void simulation(struct queue *event_queue, int vertices)
 {
+	// The first persont to be infected.
 	int first_infect = rand()%vertices;
 	enqueue(event_queue, first_infect, 1, 'T');
 	graph[first_infect].pred_inf_time = 1;
 	curr_day++;
 
+	// Event Processing
 	while((event_queue->length) && curr_day < (DAYS+1))
 	{
 		if((event_queue->front->day) > curr_day)
@@ -282,6 +307,7 @@ void simulation(struct queue *event_queue, int vertices)
 			proc_rec_SIR(event_queue,&graph[(event_queue->front->id)]);
 		}
 	}
+	// Printing the parameters of remaining days.
 	while(curr_day < (DAYS+1))
 	{
 		printf("%d,%d,%d,%d\n",curr_day,susceptible,infected,recovered);
@@ -291,7 +317,9 @@ void simulation(struct queue *event_queue, int vertices)
 
 int main()
 {
+	// Seeding the rand();
 	srand(time(NULL));
+	// Random number of vertices.
 	int num_nodes = rand()%10000;
 	create_graph(num_nodes);
 	simulation(&events,num_nodes);
